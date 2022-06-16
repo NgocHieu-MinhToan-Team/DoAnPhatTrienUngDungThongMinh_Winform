ea01cd7fe16131f586bf1134933ff7fb79ea044a
ngochieu2004 <75209627+ngochieu2004@users.noreply.github.com>
2022-06-16 21:55:20 +0700

    upd GUI

--- DTO_PPL/Properties/Settings.Designer.cs
+++ DTO_PPL/Properties/Settings.Designer.cs
@@ -55,5 +55,16 @@ namespace DTO_PPL.Properties {
                 return ((string)(this["ManageRestaurantConnectionString1"]));
             }
         }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=NGCHIUTRNC995;Initial Catalog=ManageRestaurant;Integrated Security=Tr" +
+            "ue")]
+        public string ManageRestaurantConnectionString2 {
+            get {
+                return ((string)(this["ManageRestaurantConnectionString2"]));
+            }
+        }
     }
 }
